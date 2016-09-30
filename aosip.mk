d14ecbf506bab29e4cb0f330514316c8527f000f
xlxfoxxlx <joshfox87@gmail.com>
2016-09-29 23:15:18 -0400

    Create aosip.mk

---
+++ aosip.mk
@@ -0,0 +1,17 @@
+# Inherit AOSP device configuration for shamu.
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aosip_angler
+PRODUCT_BRAND := Google
+PRODUCT_DEVICE := angler
+PRODUCT_MODEL := Nexus 6P
+PRODUCT_MANUFACTURER := Huawei
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=angler \
+    BUILD_FINGERPRINT=google/angler/angler:7.0/NRD90U/3155372:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.0 NRD90U 3155372 release-keys" \
+    BUILD_ID=NRD90U \
+    BUILD_DISPLAY_ID=NRD90U
